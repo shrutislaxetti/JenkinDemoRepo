fb4f912fd121a0a34d43e023f1126b211c93ca3d
shrutislaxetti <shrutilaxetti@gmail.com>
2018-09-18 19:43:44 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/hello/JenkinsDemoApplication.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+
+	}
+	@GetMapping("/")
+	public String getMessage() {
+		return "hello World";
+	}
+	
+}
